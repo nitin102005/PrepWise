2e1cc0d859fe548391cc1f8551b451cadb70b39c
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-06-18 12:45:22 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,7 +3,6 @@ import { googleAuth, logOut } from "../controllers/auth.controller.js"
 
 const authRouter = express.Router()
 
-
 authRouter.post("/google",googleAuth)
 authRouter.get("/logout",logOut)
 
