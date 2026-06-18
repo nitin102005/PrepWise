ab05a1b8539dcdb372af2b7efe78d1393387d0df
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-06-18 12:45:46 +0530

    Update payment.route.js

--- server/routes/payment.route.js
+++ server/routes/payment.route.js
@@ -3,7 +3,6 @@ import isAuth from "../middlewares/isAuth.js"
 import { createOrder, verifyPayment } from "../controllers/payment.controller.js"
 
 
-
 const paymentRouter = express.Router()
 
 paymentRouter.post("/order" , isAuth , createOrder )
